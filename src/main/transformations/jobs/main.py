4cfda3f4f03f5fc289b59ba702cb1d90e7741ab3
Sameer Shaik <sameersmart7865@gmail.com>
2024-08-26 12:15:35 +0530

    Ensured the process tracking by checking the local specified directory

--- src/main/transformations/jobs/main.py
+++ src/main/transformations/jobs/main.py
@@ -1,7 +1,10 @@
+import os
+
 from resources.dev import config
 from src.main.utility.encrypt_decrypt import *
 from src.main.utility.s3_client_object import *
 from src.main.utility.logging_config import *
+from src.main.utility.sql_session_script import *
 
 # =====================
 # AWS Credentials Retrieval
@@ -44,3 +47,45 @@ response = s3_client.list_buckets()
 # This will help track and verify the operation performed.
 
 logger.info("List of Buckets in our AWS : %s", response['Buckets'])
+
+""" 
+We know that: We will read the data file from s3 bucket to our local system
+and process it in our local system and then again write it to s3 bucket
+
+For this, what we will do is:
+    1. First,if we started a process to get the file from s3 and then the process is failed then we'll check if the file is downloaded locally during the process.
+    2. We check if local directory has already a file. If yes, then check if the same file is present in the staging area
+    3. If file is present in the staging area as well then dont delete it instead re-run it. If not present then give an error and process the next file.
+    Example: If the file is present in the staging area with Active stage then it means that our process is started and we got the file in our staging area still it failed,
+     So we'll try to re-run it. 
+"""
+# This csv_files object will get all the required csv files present in our local specified directory using config files
+csv_files = [file for file in os.listdir(config.local_directory) if file.endswith(".csv")]
+# We will create the connection with our mysql
+connection = get_mysql_connection()
+cursor = connection.cursor()
+
+total_csv_files =[]
+if csv_files:
+    for file in csv_files:
+        total_csv_files.append(file)
+
+    statement = """
+                select distinct file_name from
+                jiomart_dataanalysis.product_staging_table
+                where file_name in ({str(total_csv_files)[1:-1]}) and status = 'I'
+    """
+    logger.info(f"dynamically statement created: {statement} ")
+    cursor.execute(statement)
+    data = cursor.fetchall()  # fetches all the data from the sql files/tables
+
+    # To verify this, go to specified local directory and create a csv file
+    if data:
+        logger.info("Your Last run was failed please check")
+    else:
+        logger.info("No records found")
+
+else:
+    logger.info("Last run was successful!!!")
+
+# Reading files from s3
